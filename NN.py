7b509c295ecd98b32acd3ac9016febd344e0ba34
Zandros229 <ernest.figura548@gmail.com>
2019-03-14 16:18:15 +0100

    some refactoring

---
+++ NN.py
@@ -0,0 +1,31 @@
+def nearest(matrix, number_of_points):
+    used = [None] * (number_of_points + 1)
+    tab = copy.deepcopy(matrix)
+    used[0] = 0
+    road = 0
+    j = 1
+    helper = False
+    index = 0
+    helper2 = False
+    for i in range(number_of_points):
+        tab[index].sort()
+    while not helper:
+        helper2 = False
+        i = 1
+        while not helper2:
+            value = tab[index][i]
+            temp = (matrix[index] == value)
+            point = list(temp).index(True)
+            if point not in used:
+                used[j] = point
+                road += value
+                helper2 = True
+                j += 1
+                index = point
+            else:
+                i += 1
+        if used[number_of_points - 1] is not None:
+            helper = True
+            road += matrix[used[number_of_points - 1]][0]
+            used[number_of_points] = 0
+    return road
--- Tab.py
+++ Tab.py
@@ -6,6 +6,7 @@ import heapq
 import numpy
 import brute
 import dfs
+import NN
 
 
 def random_points(number_of_Points):
@@ -35,46 +36,16 @@ def distance_matrix(tab, number_of_points):
     return matrix
 
 
-def nearest(matrix, number_of_points):
-    used = [None] * (number_of_points + 1)
-    tab = copy.deepcopy(matrix)
-    used[0] = 0
-    road = 0
-    j = 1
-    helper = False
-    index = 0
-    helper2 = False
-    for i in range(number_of_points):
-        tab[index].sort()
-    while not helper:
-        helper2 = False
-        i = 1
-        while not helper2:
-            value = tab[index][i]
-            temp = (matrix[index] == value)
-            point = list(temp).index(True)
-            if point not in used:
-                used[j] = point
-                road += value
-                helper2 = True
-                j += 1
-                index = point
-            else:
-                i += 1
-        if used[number_of_points - 1] is not None:
-            helper = True
-            road += matrix[used[number_of_points - 1]][0]
-            used[number_of_points] = 0
-    return road
+
 
 
 tab = random_points(4)
 print(tab)
 # NN
 matrix = distance_matrix(tab, 4)
-near = nearest(matrix, 4)
+near = NN.nearest(matrix, 4)
 print(matrix)
-print(nearest(matrix, 4))
+print(NN.nearest(matrix, 4))
 # Brute Force
 
 perm_tabel = brute.perm(4)
